42ac3ba1c16e9887c45bf1baf0d0262cb1a1e077
TannerBarrett <90267759+TannerBarrett@users.noreply.github.com>
2021-10-22 09:36:15 -0600

    o

---
+++ scripts/whatEmojisDo.js
@@ -0,0 +1,31 @@
+function poop() {
+  console.log('poop')
+}
+
+function angry() {
+  console.log('angry')
+}
+
+function sad() {
+  console.log('sad')
+}
+
+function devil() {
+  console.log('devil')
+}
+
+function love() {
+  console.log('luv')
+}
+
+function heartEye() {
+  console.log('hi')
+}
+
+function unicorn() {
+  console.log('uni')
+}
+
+function happy() {
+  console.log('hapy')
+}
