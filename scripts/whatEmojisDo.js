8e64d87bef463460dd61547a431dbf2d6511a852
TannerBarrett <90267759+TannerBarrett@users.noreply.github.com>
2021-10-22 10:08:23 -0600

    o

--- scripts/whatEmojisDo.js
+++ scripts/whatEmojisDo.js
@@ -1,31 +1,43 @@
+// global variables
+let currentEmojiEl = document.getElementById('current-emoji')
+let infoEl = document.getElementById('info')
+// declare functions
 function poop() {
-  console.log('poop')
+  currentEmojiEl.src = '../images/poop-emoji.jpg'
+  infoEl.innerHTML = 'Both your score and your gold amount increase by your current clicking power'
 }
 
 function angry() {
-  console.log('angry')
+  currentEmojiEl.src = '../images/angry-emoji.png'
+  infoEl.innerHTML = 'Your gold amount decreases by 1.5* your current clicking power. Ex 1 --> 1.5, 4 --> 6, 10 --> 15.'
 }
 
 function sad() {
-  console.log('sad')
+  currentEmojiEl.src = '../images/sad-emoji.png'
+  infoEl.innerHTML = 'Your gold amount decreases by your current clicking power'
 }
 
 function devil() {
-  console.log('devil')
+  currentEmojiEl.src = '../images/devil-emoji.png'
+  infoEl.innerHTML = 'Your score decreases by 0.75* your total score. Ex: 1 --> .75, 100 --> 75, 1000 --> 750.'
 }
 
 function love() {
-  console.log('luv')
+  currentEmojiEl.src = '../images/love-emoji.webp'
+  infoEl.innerHTML = 'Both your score and your gold amount increase by your current clicking power'
 }
 
 function heartEye() {
-  console.log('hi')
+  currentEmojiEl.src = '../images/heart-eye-emoji.png'
+  infoEl.innerHTML = 'Both your score and your gold amount increase by your current clicking power'
 }
 
 function unicorn() {
-  console.log('uni')
+  currentEmojiEl.src = '../images/unicorn.png'
+  infoEl.innerHTML = 'Both your score and your gold amount increase by your current clicking power'
 }
 
 function happy() {
-  console.log('hapy')
+  currentEmojiEl.src = '../images/happy emoji.jpg'
+  infoEl.innerHTML = 'Both your score and your gold amount increase by your current clicking power'
 }
