2e38cd761e9e1481b5386045e213fb93edee1486
TannerBarrett <90267759+TannerBarrett@users.noreply.github.com>
2021-10-28 10:00:02 -0600

    o

--- scripts/reasonToPlay.js
+++ scripts/reasonToPlay.js
@@ -1,9 +1,40 @@
-let i = 10
+  let i = 6
 function timer(){
-  if (i <= 0) {
+  if (i > 0) {
     i--
     console.log(i)
-
   }
+document.getElementById('timer').innerHTML = i
+  if (i == 0) {
+  redirect()
+  }
+}
+
+function no(){
   setInterval(timer, 1000)
+  document.getElementById('no').onclick = ''
+}
+
+function redirect(){
+  window.location.href = "../html/run.html";
+}
+
+function timer2(){
+  if (i > 0) {
+    i--
+    console.log(i)
+  }
+document.getElementById('timer').innerHTML = i
+  if (i == 0) {
+  redirect2()
+  }
+}
+
+function yes(){
+  setInterval(timer2, 1000)
+  document.getElementById('yes').onclick = ''
+}
+
+function redirect2(){
+  window.location.href = "../html/link.html";
 }
